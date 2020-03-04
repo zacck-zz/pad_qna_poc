cfba3e0bb0f1bfdf336b66c8f255eea6a8fd6f0f
Zacck Osiemo <coderv63@gmail.com>
2020-03-04 16:14:04 +0300

    Build initial POC to  consume file from JS to elm

---
+++ src/Main.elm
@@ -0,0 +1,71 @@
+port module Main exposing (main)
+
+import Browser
+import Browser.Navigation as Nav
+import File exposing (File)
+import Html exposing (Html, button, div, h1, text)
+import Html.Events exposing (onClick)
+import Url exposing (..)
+
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
+
+init : () -> ( Model, Cmd Msg )
+init _  =
+  ({answer = Nothing }, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
+
+type alias Model =
+  { answer : Maybe String }
+
+type Msg =
+  StartRecording
+  | StopRecording
+  | UploadAnswer String
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    StartRecording ->
+      let
+          start =
+            startRecording ()
+      in
+      (model, start)
+
+    StopRecording ->
+      let
+          stop =
+            stopRecording ()
+      in
+      (model, stop)
+
+    UploadAnswer ans ->
+      let
+          newModel = { model | answer = Just ans}
+      in
+      (newModel, Cmd.none)
+view : Model -> Html Msg
+view model =
+        div []
+        [ h1 [] [ text "Answer Recorder" ]
+        , button [ onClick StartRecording ] [ text "Record"]
+        , h1 [] [ text "            " ]
+        , button [ onClick StopRecording] [ text "Stop" ]
+        ]
+
+
+port startRecording : () -> Cmd msg
+
+port stopRecording : () -> Cmd msg
+
+port consumeAudio : (String -> msg) -> Sub msg
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import { Elm } from './Main.elm'
+
+let mediaRecorder;
+let recordedAudio =  [];
+
+const app = Elm.Main.init({
+  node: document.querySelector('main')
+})
+
+app.ports.startRecording.subscribe(function() {
+	console.log("Now we record audio");
+	recordAudio();
+})
+
+app.ports.stopRecording.subscribe(function() {
+	console.log("Now we are done recording audio");
+	console.log(mediaRecorder.state);
+	mediaRecorder.stop();
+	console.log(mediaRecorder.state);
+	// make blob of our data
+	var file = new Blob(recordedAudio, {type: 'audio/wav'});
+	const audioUrl = URL.createObjectURL(file);
+	console.log(app)
+
+	//const audio = new Audio(audioUrl);
+	//audio.play();
+
+})
+
+
+// set up user media
+function recordAudio() {
+	navigator.mediaDevices.getUserMedia({
+		audio: true
+	})
+	.then(function(stream) {
+		mediaRecorder = new MediaRecorder(stream);
+		// configure our chunker
+		mediaRecorder.ondataavailable = handleRecordedData;
+		mediaRecorder.start();
+		console.log(mediaRecorder.state);
+		console.log("recorder started");
+	})
+}
+
+
+// handle recorded stream
+function handleRecordedData(event) {
+	if (event.data.size > 0) {
+		recordedAudio.push(event.data)
+	} else {
+		console.log("something went saving chunks")
+		console.log(event)
+	}
+}
+
+
+
+
+
+
