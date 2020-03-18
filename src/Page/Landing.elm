f87bd08273f0d39588f4afba06bf04c3502f0b34
Zacck Osiemo <coderv63@gmail.com>
2020-03-18 17:12:02 +0300

    Move the landing page to its own module/file

---
+++ src/Page/Landing.elm
@@ -0,0 +1,63 @@
+module Page.Landing exposing (Model, Msg, init,  view)
+
+
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+import Profile exposing (phoneToString)
+import Session exposing (Session)
+import Route exposing (Route)
+
+
+type alias Model =
+  { queue : String
+  , session : Session
+  }
+
+type Msg =
+  SetQueue String
+
+init : Session -> (Model, Cmd Msg)
+init sess =
+  let
+      model =
+        { session = sess
+        , queue = ""
+        }
+
+      cmd =
+        case Session.cred sess of
+          Just cred ->
+            let
+               maybePhone =
+                sess
+                |> Session.phoneString
+                |> Just
+            in
+            Route.replaceUrl (Session.navKey sess) (Route.AnswerDashboard maybePhone)
+
+          Nothing ->
+            Cmd.none
+  in
+      (model, cmd)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    SetQueue q ->
+      ({ model | queue = q }, Cmd.none)
+
+
+view : Model -> { title : String, body : Html msg }
+view _ =
+  let
+      b =
+        div [ class "container"]
+            [ text "Landing Page" ]
+  in
+      { title  = "Welcome"
+      , body = b
+      }
+
+
+
