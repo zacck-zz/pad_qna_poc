41144a9b3aa0c25ec867b50be20f40fc42f7b931
Zacck Osiemo <coderv63@gmail.com>
2020-03-19 14:48:42 +0300

    Build state machine to handle Landing page logic

--- src/Page/Landing.elm
+++ src/Page/Landing.elm
@@ -1,8 +1,9 @@
-module Page.Landing exposing (Model, Msg, init, toSession, update, view)
+module Page.Landing exposing (Model, Msg, init, subscriptions, toSession, update, view)
 
 
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, input, label, text)
+import Html.Attributes exposing (class, type_)
+import Html.Events exposing (onClick, onInput)
 import Profile exposing (phoneToString)
 import Session exposing (Session)
 import Route exposing (Route)
@@ -12,9 +13,6 @@ type alias Model =
   { queue : String
   , session : Session
   }
-
-type Msg =
-  SetQueue String
 
 init : Session -> (Model, Cmd Msg)
 init sess =
@@ -41,19 +39,68 @@ init sess =
       (model, cmd)
 
 
+type Msg =
+  SetQueue String
+  | GoToQueue
+  | GotSession Session
+
+
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     SetQueue q ->
-      ({ model | queue = q }, Cmd.none)
+      ( { model | queue = q }
+      , Cmd.none
+      )
+
+    GoToQueue ->
+      if String.isEmpty model.queue then
+        (model, Cmd.none)
+      else
+        let
+            profileData =
+              { phone = model.queue }
+
+            loginData =
+              { token = model.queue
+              , user = profileData
+              }
+        in
+            ( model
+            , Session.login loginData
+            )
+
+    GotSession sess ->
+      let
+          updatedModel =
+            { model | session = sess }
+
+          routeCmd =
+            model.queue
+            |> Just
+            |> Route.AnswerDashboard
+            |> Route.replaceUrl (Session.navKey sess)
+      in
+          (updatedModel, routeCmd)
 
 
-view : Model -> { title : String, body : Html msg }
+view : Model -> { title : String, body : Html Msg }
 view _ =
   let
       b =
         div [ class "container"]
-            [ text "Landing Page" ]
+            [  div [ class "landing-page" ]
+                   [ div [ class "file-form"]
+                          [ label [] [ text "Phone Number:" ]
+                          , input [ type_ "text", onInput SetQueue] []
+                          ]
+                   , div [ class "file-form"]
+                         [ button [] [ text "Master" ]
+                         , button [ onClick GoToQueue ] [ text "Set Queue" ]
+                         ]
+                   ]
+            ]
   in
       { title  = "Welcome"
       , body = b
@@ -64,3 +111,7 @@ toSession : Model -> Session
 toSession { session } =
   session
 
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Session.changes GotSession (Session.navKey model.session)
